d0ecaae521265286a102fbf47c6291956f1ef964
16bh <xujun_njupt@126.com>
2017-05-17 23:42:14 +0800

    后台管理

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//写表用post 读表用get
 Route::get('/','ArticleController@index');
 Route::get('articles/{id}','ArticleController@show')
 ->where('id', '[0-9]+');
@@ -19,11 +20,27 @@ Route::get('article/create','ArticleController@create');
 Route::post('article/store','ArticleController@store');
 Route::get('article/edit/{id}','ArticleController@edit');
 Route::post('article/update','ArticleController@update');
-// Route::get('/', function () {
-//     return 'HelloWorld!';
-//     return view('welcome');
-// });
 
-// Route::get('user/{name}', function ($name) {
-//     return 'Hello '.$name;
-// });
+
+// Admin area
+Route::get('admin', function () {
+    return redirect('/admin/post');
+});
+
+//路由群组
+//namespace 命名空间
+//middleware 中间件
+$router->group(['namespace' => 'Admin', 'middleware' => 'auth'], function () {
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::get('admin/upload', 'UploadController@index');
+});
+
+//Logging in and out
+Route::get('/login', 'Auth\AuthController@getLogin');
+Route::post('/login', 'Auth\AuthController@postLogin');
+Route::get('/logout', 'Auth\AuthController@getLogout');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
